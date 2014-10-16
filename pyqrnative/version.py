650b959771a57139009fc179b017897e4f2f7015
Joe White <joe@freicoin.us>
2014-10-15 22:46:53 -0400

    Initial commit - no sending
    initial commit -- nightly with no sending

---
+++ pyqrnative/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.1.4'
+version = '0.1.4'
+version_major = 0
+version_minor = 1
+version_micro = 4
+release = True
+
+if not release:
+    version += '.dev9f5592583c34bb44501cd4d4de566260b09cccab'
